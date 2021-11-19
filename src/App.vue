43d1238c75d55cd79cf60dd774390625de1a4be7
imraan024 <asq24i@gmail.com>
2021-11-19 21:37:23 +0600

    Homepage running

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="wrapper">
+  
+  <nav class="navbar is-dark">
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item"><strong>Hoodie Shop</strong></router-link>
+      <a class="navbar-burger" area-label="menu" aria-expanded="false" data-target = "navbar-menu">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div class="navbar-menu" id="navbar-menu">
+      <div class = "navbar-end">
+        <router-link to="/winter" class="navbar-item">Winter</router-link>
+        <router-link to="/summer" class="navbar-item">Summer</router-link>
+        <div class="navbar-item">
+          <router-link to="/log-in" class="button is-success">Log In</router-link>
+          <router-link to="/cart" class="button is-dark">
+            <span class="icon">
+              <i class="fas fa-shopping-cart"></i>
+            </span>
+            <span>Cart</span>
+          </router-link>
+        </div>
+      </div>
+
+    </div>
+  </nav>
+  
+  <section class="section">
+    <div class="container">
+      <Home/>
+      <router-view></router-view>
+    </div>
+  </section>
+  <footer class="footer">
+    <p class="has-text-centered">Copyright</p>
+
+  </footer>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from './views/Home';
 export default {
-  name: 'App',
+  name: 'app',
+  setup() {
+
+  },
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import '../node_modules/bulma';
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="home">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                    Hoodie Shop
+                </h1>
+                <h2 class="subtitle">
+                    Winter is coming
+                </h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    default(){
+        
+    },
+    components:{
+    }
+}
+</script>
+
+<style>
+
+</style>
